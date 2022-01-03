5fc0080517ee49f84c86202d30fafb3854f714bd
NashidForbes <NashidForbes@biohumans.com>
2022-01-02 23:08:39 -0500

    Updating UserService to be a proper Eureka client with database settings for microservice design pattern.

--- src/main/java/com/appsdeveloperblog/estore/usersservice/UsersServiceApplication.java
+++ src/main/java/com/appsdeveloperblog/estore/usersservice/UsersServiceApplication.java
@@ -2,7 +2,11 @@ package com.appsdeveloperblog.estore.usersservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+//TODO register this microservice with Eureka server
+//with it's own database
+@EnableDiscoveryClient
 @SpringBootApplication
 public class UsersServiceApplication {
 
